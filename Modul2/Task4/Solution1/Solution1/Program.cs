d53fc97c0e48b845dac60ff21ef8535d9792cdc1
PavelKorolev <80217659+PavelKorolev@users.noreply.github.com>
2021-12-13 17:17:49 +0300

    Add files via upload

---
+++ Modul2/Task4/Solution1/Solution1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Solution1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day of the week:\n 1 - Monday\n 2 - Tuesday\n 3 - Wednesday\n 4 - Thursday\n 5 - Friday\n 6 - Saturday\n 7 - Sunday");
+            byte day = Convert.ToByte(Console.ReadLine());
+            
+            while (day > 0 && day < 8)
+            {
+                switch (day)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Monday");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Tuesday");
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Wednesday");
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Thursday");
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("Friday");
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.WriteLine("Saturday");
+                            break;
+                        }
+                    case 7:
+                        {
+                            Console.WriteLine("Sunday");
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
