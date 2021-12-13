60001a557172008fad25d83326543ff44200aea2
PavelKorolev <80217659+PavelKorolev@users.noreply.github.com>
2021-12-13 17:33:00 +0300

    Add files via upload

---
+++ Modul6/Task16/Solution1/Solution1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Solution1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] AS;
+            int count;
+            string s;
+            string[] AS2;
+
+            count = Convert.ToInt32(Console.ReadLine());
+
+            AS = new string[count];
+
+
+            for (int i = 0; i < AS.Length; i++)
+                AS[i] = Console.ReadLine() ?? string.Empty;
+
+
+
+            for (int i = 0; i < AS.Length; i++)
+            {
+                if (AS[i] == string.Empty)
+                {
+                    AS[i] = "String is sempty";
+                    //Console.WriteLine("String is empty");
+                }
+            }
+
+            for (int i = 0; i < AS.Length; i++)
+                Console.WriteLine(AS[i]); ;
+        }
+    }
+}
---
+++ Modul6/Task18/Solution1/Solution1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Solution1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DayWithEnum(5));
+        }
+
+        public static DayOfWeek? DayWithEnum(int day)
+        {
+            if (day == 1)
+            {
+                return DayOfWeek.Monday;
+            }
+            else if (day == 2)
+            {
+                return DayOfWeek.Tuesday;
+            }
+            else if (day == 3)
+            {
+                return DayOfWeek.Wednesday;
+            }
+            else if (day == 4)
+            {
+                return DayOfWeek.Thursday;
+            }
+            else if (day == 5)
+            {
+                return DayOfWeek.Friday;
+            }
+            else if (day == 6)
+            {
+                return DayOfWeek.Saturday;
+            }
+            else if (day == 7)
+            {
+                return DayOfWeek.Sunday;
+            }
+           /* else if (day == 0)
+            {
+                return (DayOfWeek?)0;
+            }*/
+            else
+            {
+                return (DayOfWeek?)day;
+            }
+        }
+    }
+}
