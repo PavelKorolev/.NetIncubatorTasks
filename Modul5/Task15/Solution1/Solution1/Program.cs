009d2b1fb0a48212abd92b8b6d3466fcf6a24fce
PavelKorolev <80217659+PavelKorolev@users.noreply.github.com>
2021-12-13 17:31:29 +0300

    Add files via upload

---
+++ Modul5/Task15/Solution1/Solution1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Solution1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Delete("I like like learn learn c#.");
+        }
+
+        static void Delete(string str)
+        {
+            string[] words = str.Split(' ');
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                if (words[i] == words[i + 1])
+                {
+                    words[i] = string.Empty;
+                }
+            }
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] == string.Empty)
+                {
+                    continue;
+                }
+                Console.Write(words[i] + " ");
+            }
+
+        }
+    }
+}
---
+++ Modul5/Task15/Solution3/Solution3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Solution2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseText("How are you?");
+            //DeleteSpaces();
+        }
+
+        static void ReverseText(string text)
+        {
+            string txt = "";
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                txt += text[i];
+            }
+            Console.WriteLine(txt);
+        }
+
+        /*static void DeleteSpaces(string text)
+        {
+           
+        }*/
+    }
+}
