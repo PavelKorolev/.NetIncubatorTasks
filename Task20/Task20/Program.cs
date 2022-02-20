eec61368e9d36b5a8714b5202989b0bd54106846
PavelKorolev <80217659+PavelKorolev@users.noreply.github.com>
2022-02-20 19:46:07 +0300

    Add files via upload

---
+++ Task20/Task20/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace School_Task
+{
+    public class Student
+    {
+        public readonly double gradePoint;
+        private string lastName;
+        private string name;
+        private int schoolNumber;
+
+        private static int numberOfStudents = 0;
+        private static double pointsSum = 0;
+        private static double pointsAverage;
+
+        static Student()
+        {
+            Console.WriteLine($"Static constructor execution {numberOfStudents}");
+        }
+
+        public Student(string LastName, string Name, double GradePoint, int SchoolNumber = 51)
+        {
+            lastName = LastName;
+            name = Name;
+            gradePoint = GradePoint;
+            schoolNumber = SchoolNumber;
+
+            numberOfStudents++;
+            pointsSum += GradePoint;
+            Console.WriteLine($"Default constructor execution {numberOfStudents}");
+        }
+
+        public static void DisplayStudentsCount()
+        {
+            Console.WriteLine(numberOfStudents);
+        }
+
+        public static double CountAverage()
+        {
+            pointsAverage = pointsSum / numberOfStudents;
+            return pointsAverage;
+        }
+
+        public override string ToString()
+        {
+            return $"{lastName} {name} {gradePoint}";
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            List<Student> students = new();
+
+            for (int i = 0; i < N; i++)
+            {
+                string LastName = Console.ReadLine();
+                string Name = Console.ReadLine();
+                double GradePoint = double.Parse(Console.ReadLine().Replace('.', ','));
+                Student tmpStudent = new Student(LastName, Name, GradePoint);
+                students.Add(tmpStudent);
+            }
+            Console.WriteLine(Student.CountAverage());
+            foreach (var student in students)
+            {
+                if (student.gradePoint < Student.CountAverage())
+                {
+                    Console.WriteLine(student.ToString());
+                }
+            }
+        }
+    }
+}
